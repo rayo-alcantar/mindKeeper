0794008d54635dfa443f01dfec324a52e6d00492
Angel Alcantar <rayoalcantar@gmail.com>
2025-03-17 09:00:02 -0600

    se corrige la interfaz principal

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,4 @@
-﻿//lib/screens/home_screen.dart
+﻿// lib/screens/home_screen.dart
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -7,7 +7,6 @@ class HomeScreen extends StatelessWidget {
   Future<void> _openUrl(BuildContext context, String urlString) async {
     final Uri url = Uri.parse(urlString);
     if (await canLaunchUrl(url)) {
-      // Se abre la URL en una aplicación externa.
       await launchUrl(url, mode: LaunchMode.externalApplication);
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -16,55 +15,75 @@ class HomeScreen extends StatelessWidget {
     }
   }
 
-  // Función para salir de la aplicación.
-  void _exitApp(BuildContext context) {
-    Navigator.of(context).pop();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Se asigna un fondo con color personalizado
+      backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
         title: Text('MindKeeper'),
       ),
       body: ListView(
         padding: EdgeInsets.all(16.0),
         children: [
+          // Botón para Crear recordatorio
           ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blueAccent,
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+            ),
             onPressed: () {
               Navigator.pushNamed(context, '/create');
             },
             child: Text('Crear recordatorio'),
           ),
+          SizedBox(height: 16),
+          // Botón para Gestionar recordatorios
           ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.deepPurple,
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+            ),
             onPressed: () {
               Navigator.pushNamed(context, '/manage');
             },
             child: Text('Gestionar recordatorios'),
           ),
+          SizedBox(height: 16),
+          // Botón para Configuración
           ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.teal,
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+            ),
             onPressed: () {
               Navigator.pushNamed(context, '/config');
             },
             child: Text('Configuración'),
           ),
+          SizedBox(height: 16),
+          // Botón para Donar al desarrollador
           ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.orange,
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+            ),
             onPressed: () {
               _openUrl(context, 'https://paypal.me/rayoalcantar?country.x=MX&locale.x=es_XC');
             },
             child: Text('Donar al desarrollador'),
           ),
+          SizedBox(height: 16),
+          // Botón para Mirar la web del desarrollador
           ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.indigo,
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+            ),
             onPressed: () {
               _openUrl(context, 'https://rayoscompany.com/');
             },
             child: Text('Mirar la web del desarrollador'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              _exitApp(context);
-            },
-            child: Text('Salir'),
           ),
         ],
       ),
